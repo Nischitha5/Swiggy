da9eef8a0d99f2b2ece19f755d3a16870af13693
Nischitha <nischithae@gmail.com>
2023-02-17 10:06:29 +0530

    made changes

--- src/test/java/utils/TestDataReader.java
+++ src/test/java/utils/TestDataReader.java
@@ -8,7 +8,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
-
 public class TestDataReader {
     private static HashMap<String, HashMap<String, String>> data;
 
